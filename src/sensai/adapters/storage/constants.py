72c832fa4ba5e245d8b6a9b1b64af316715526a2
Charles Mahoudeau <charles@mahoudeau.eu>
2026-09-25 15:46:31 +0200

    refactor(storage): convert `DB_FILE` to `Path` type for consistency

--- src/sensai/adapters/storage/constants.py
+++ src/sensai/adapters/storage/constants.py
@@ -2,6 +2,6 @@
 
 from pathlib import Path
 
-DB_FILE: str = "sensai.db"
+DB_FILE: Path = Path("sensai.db")
 MIGRATIONS_DIR: Path = Path(__file__).parent.joinpath("migrations")
 MIGRATIONS_START_ID: int = 0
