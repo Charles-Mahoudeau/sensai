37da0a967446c1e6af98437f47d7bb669d96fa8f
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-09-25 12:38:02 +0200

    feat(exceptions.py): add an error class for events models

--- src/sensai/exceptions.py
+++ src/sensai/exceptions.py
@@ -1,6 +1,10 @@
 """Project's root exceptions."""
 
 
+class SensaiError(Exception):
+    """Base class for all Sensai-related errors."""
+
+
 class ConfigError(Exception):
     """Raised when a configuration error occurs."""
 
